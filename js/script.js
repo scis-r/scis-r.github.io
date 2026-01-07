7dfb0461431cadad5ed4a5d813abfa51603d5a0a
scis-r <danilo.oblenis@gmail.com>
2026-01-07 18:33:27 -0500

    github fix
    moved the html files to the root folders so that github pages will actually display the website

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 function randomBackgroundTile(numChoices) {
     const number = Math.floor(Math.random() * numChoices) + 1;
-    let urlPath = "../images/bg_tile";
+    let urlPath = "images/bg_tile";
     const numberString = number.toString();
     urlPath = urlPath.concat(numberString, ".png");
     document.body.style.backgroundImage = `url(${urlPath})`;
